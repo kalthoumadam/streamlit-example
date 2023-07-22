874e56dd086abe704a8d197aa8c7066c2f489ef3
kalthoumadam <66341942+kalthoumadam@users.noreply.github.com>
2023-07-22 16:56:43 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,56 @@ import altair as alt
 import math
 import pandas as pd
 import streamlit as st
+import plotly.express as px
+
+st.set_page_config(layout="wide")
+
+# -- Create three columns
+col1, col2, col3 = st.columns([5, 5, 20])
+
+with col3:
+    st.title("Streamlit Demo")
+
+
+year_col, continent_col, log_x_col = st.columns([5, 5, 5])
+with year_col:
+    year_choice = st.slider(
+        "What year would you like to examine?",
+        min_value=1952,
+        max_value=2007,
+        step=5,
+        value=2007,
+    )
+with continent_col:
+    continent_choice = st.selectbox(
+        "What continent would you like to look at?",
+        ("All", "Asia", "Europe", "Africa", "Americas", "Oceania"),
+    )
+with log_x_col:
+    log_x_choice = st.checkbox("Log X Axis?")
+
+# -- Read in the data
+df = px.data.gapminder()
+# -- Apply the year filter given by the user
+filtered_df = df[(df.year == year_choice)]
+# -- Apply the continent filter
+if continent_choice != "All":
+    filtered_df = filtered_df[filtered_df.continent == continent_choice]
+
+# -- Create the figure in Plotly
+fig = px.scatter(
+    filtered_df,
+    x="gdpPercap",
+    y="lifeExp",
+    size="pop",
+    color="continent",
+    hover_name="country",
+    log_x=log_x_choice,
+    size_max=60,
+)
+fig.update_layout(title="GDP per Capita vs. Life Expectancy")
+# -- Input the Plotly chart to the Streamlit interface
+st.plotly_chart(fig, use_container_width=True)
 
 """
 # Welcome to Streamlit!
